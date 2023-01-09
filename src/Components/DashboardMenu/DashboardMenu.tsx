b04d1071e26b3378bacb0571117714e97920f911
chris3evans <chris3evans.ce@gmail.com>
2023-01-09 16:14:15 +0000

    [Style] - Added styling to dashboard menu component

--- src/Components/DashboardMenu/DashboardMenu.tsx
+++ src/Components/DashboardMenu/DashboardMenu.tsx
@@ -1,7 +1,37 @@
 import styles from "./DashboardMenu.module.css";
 
 const DashboardMenu = function () {
-  return <div></div>;
+  return (
+    <div className={styles["dashboard-menu"]}>
+      <ul className={styles["dashboard-menu-list"]}>
+        <li className={styles["dashboard-menu-item"]}>
+          <button type="button" className={styles["dashboard-menu-button"]}>
+            <h2 className={styles["dashboard-menu-text"]}>Profile</h2>
+          </button>
+        </li>
+        <li className={styles["dashboard-menu-item"]}>
+          <button type="button" className={styles["dashboard-menu-button"]}>
+            <h2 className={styles["dashboard-menu-text"]}>Find Brands</h2>
+          </button>
+        </li>
+        <li className={styles["dashboard-menu-item"]}>
+          <button type="button" className={styles["dashboard-menu-button"]}>
+            <h2 className={styles["dashboard-menu-text"]}>My Offers</h2>
+          </button>
+        </li>
+        <li className={styles["dashboard-menu-item"]}>
+          <button type="button" className={styles["dashboard-menu-button"]}>
+            <h2 className={styles["dashboard-menu-text"]}>My Sponsors</h2>
+          </button>
+        </li>
+        <li className={styles["dashboard-menu-item"]}>
+          <button type="button" className={styles["dashboard-menu-button"]}>
+            <h2 className={styles["dashboard-menu-text"]}>Settings</h2>
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default DashboardMenu;
