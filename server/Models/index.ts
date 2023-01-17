6be063c403defcdef67b9d7bd73ebd16575ecd39
chris3evans <chris3evans.ce@gmail.com>
2023-01-17 09:52:13 +0000

    [Feature] - built mongoose database connection

---
+++ server/Models/index.ts
@@ -0,0 +1,17 @@
+// const mongoose = require("mongoose");
+// // const path = require("path");
+
+// // require("dotenv").config({
+// //   path: "../../.env",
+// // });
+
+// // async function main() {
+// //   await mongoose.connect(process.env.DB_URI);
+// //   console.log("Mongoose connection to database established 📗📗📗");
+// // }
+
+// // main().catch((error) => {
+// //   console.log(`Error connecting to mongoDB database: ${error}`);
+// // });
+
+// module.exports = mongoose;
--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,23 @@ const app = Express();
 const cors = require("cors");
 const PORT = 3000;
 const Router = require("./router.ts");
+const mongoose = require("mongoose");
+
+const path = require("path");
+
+require("dotenv").config({
+  path: "../.env",
+});
 
 app.use(Express.json());
 app.use(cors());
 app.use(Router);
 
-app.listen(PORT, () => {
-  console.log(`Express server is running on port ${PORT} ✅✅✅`);
-});
+(async () => {
+  await mongoose.connect(process.env.DB_URI);
+  console.log("Mongoose connection to database established 📗📗📗");
+
+  app.listen(PORT, () => {
+    console.log(`Express server is running on port ${PORT} ✅✅✅`);
+  });
+})();
